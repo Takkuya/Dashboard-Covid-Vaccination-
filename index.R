33387bd55aa61f6468307a84e42094b7f1b4ae83
Takkuya <takuyagabriel@gmail.com>
2022-03-15 14:19:50 -0300

    More plots

--- index.R
+++ index.R
@@ -19,10 +19,11 @@ etniaAgrupado <- vacinaDados %>% group_by(paciente_racaCor_valor) %>% summarise(
 #quantidade por dose da vacina
 doseVacinaAgrupado <- vacinaDados %>% group_by(vacina_descricao_dose) %>% summarise(count = n())
 
+#porcentagem gênero
+sexo <- vacinaDados %>% filter(paciente_enumSexoBiologico %in% c("M", "F")) %>% group_by(paciente_enumSexoBiologico) %>% count() %>% ungroup() %>% mutate(porcentagem = round(n/sum(n),4)*100,  lab.pos = cumsum(porcentagem) -.5*porcentagem)
 
-#testando o plot usando apenas o ggplot
-#ggplot(etniaAgrupado, aes(x=etniaAgrupado$paciente_racaCor_valor, y=etniaAgrupado$count)) + geom_bar(stat = "identity")
-
+#quantidade marca vacina
+marcasVacina <- vacinaDados %>% group_by(vacina_fabricante_nome) %>% summarise(count = n())
 
 ui <- dashboardPage(
    dashboardHeader(title = "Dashboard covid19"),
@@ -31,24 +32,33 @@ ui <- dashboardPage(
    ),
    dashboardBody(
       fluidRow(valueBox(quantidadeVacinacao, "Pessoas vacinadas")),
-      fluidRow(column(7,plotOutput("etnia"))),
-      fluidRow(column(7, plotOutput("vacinaDose"))),
+      fluidRow(column(6,plotOutput("etnia")), column(6, plotOutput("vacinaDose"))),
+      fluidRow(column(6,plotOutput("marcas")), column(6, plotOutput("genero"))),
    )
 )
 
 server <- function(input, output) {
-   output$menu <- renderMenu({
-      sidebarMenu(
-         menuItem("Menu item", icon = icon("calendar"))
-      )
-   })
    
    output$etnia <- renderPlot({
-      ggplot(data = etniaAgrupado, aes(x = paciente_racaCor_valor, y = count)) + geom_bar(stat = "identity")
+      ggplot(data = etniaAgrupado, aes(x = paciente_racaCor_valor, y = count, fill = paciente_racaCor_valor)) + geom_bar(stat = "identity")
    })
    
    output$vacinaDose <- renderPlot({
-      ggplot(data = doseVacinaAgrupado, aes(x = vacina_descricao_dose, y = count)) + geom_bar(stat = "identity")
+      ggplot(data = doseVacinaAgrupado, aes(x = vacina_descricao_dose, y = count, fill = vacina_descricao_dose)) + geom_bar(stat = "identity")
+   })
+   
+   output$marcas <- renderPlot({
+      ggplot(data = marcasVacina, aes(x = vacina_fabricante_nome, y = count, fill = vacina_fabricante_nome)) + geom_bar(stat = "identity")
+   })
+   
+   output$genero <- renderPlot({
+      ggplot(data = sexo, 
+             aes(x = "", y = porcentagem, fill = paciente_enumSexoBiologico))+
+         geom_bar(stat = "identity")+
+         coord_polar("y", start = 200) +
+         geom_text(aes(y = lab.pos, label = paste(porcentagem,"%", sep = "")), col = "white") +
+         theme_minimal() + ggtitle( "Sexos das pessoas vacinadas" ) +
+         scale_fill_brewer(palette = "Paired")
    })
 }
 
